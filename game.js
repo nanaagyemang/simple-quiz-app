61af3b2ae730d0bcf1c732812eddb66c65780e93
nanaagyemang <nanaagyemang2058@gmail.com>
2022-01-26 23:25:02 -0800

    progress 2

---
+++ game.js
@@ -0,0 +1,113 @@
+const question = document.querySelector('#question');
+const choices = Array.from(document.querySelectorAll('.choice-text'));
+const progressText = document.querySelector('#progressText');
+const scoreText = document.querySelector('#score');
+const progressBarFull = document.querySelector('#progressBarFull');
+
+let currentQuestion = {}
+let acceptingAnswers = true
+let score = 0
+let questionCounter = 0
+let availableQuestions = []
+
+let questions = [
+    {
+        question: 'What is the capital of Ghana?',
+        choice1: 'Kumasi',
+        choice2: 'Accra',
+        choice3: 'Sunyani',
+        choice4: 'Tema',
+        answer: 2,
+    },
+    {
+        question:
+            "Which country won the 2018 World Cup?",
+        choice1: "France",
+        choice2: "India",
+        choice3: "England",
+        choice4: "None of the above",
+        answer: 1,
+    },
+    {
+        question: "Which country can be found in Africa?",
+        choice1: "Haiti",
+        choice2: "Jamaica",
+        choice3: "Mauritius",
+        choice4: "Nicaragua",
+        answer: 3,
+    },
+    {
+        question: "Select the most intelligent?",
+        choice1: "Dolphin",
+        choice2: "Goat",
+        choice3: "Sheep",
+        choice4: "Zebra",
+        answer: 1,
+    }
+]
+
+const SCORE_POINTS = 100
+const MAX_QUESTIONS = 4
+
+startGame = () => {
+    questionCounter = 0
+    score = 0
+    availableQuestions = [...questions]
+    getNewQuestion()
+}
+
+getNewQuestion = () => {
+    if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score)
+
+        return window.location.assign('/end.html')
+    }
+
+    questionCounter++
+    progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
+    progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`
+    
+    const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
+    currentQuestion = availableQuestions[questionsIndex]
+    question.innerText = currentQuestion.question
+
+    choices.forEach(choice => {
+        const number = choice.dataset['number']
+        choice.innerText = currentQuestion['choice' + number]
+    })
+
+    availableQuestions.splice(questionsIndex, 1)
+
+    acceptingAnswers = true
+}
+
+choices.forEach(choice => {
+    choice.addEventListener('click', e => {
+        if(!acceptingAnswers) return
+
+        acceptingAnswers = false
+        const selectedChoice = e.target
+        const selectedAnswer = selectedChoice.dataset['number']
+
+        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
+
+        if(classToApply === 'correct') {
+            incrementScore(SCORE_POINTS)
+        }
+
+        selectedChoice.parentElement.classList.add(classToApply)
+
+        setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(classToApply)
+            getNewQuestion()
+
+        }, 1000)
+    })
+})
+
+incrementScore = num => {
+    score +=num
+    scoreText.innerText = score
+}
+
+startGame()
